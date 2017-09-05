40973806bd55e6c0778c8cf96d51c5f73c997b74
Mengistuyes <itEthiopian@gmail.com>
2017-09-04 22:57:15 -0500

    Summary
    Math.PI

--- src/problem1/Prog1.java
+++ src/problem1/Prog1.java
@@ -6,14 +6,14 @@ public class Prog1 {
 		// 1. get a random number x in the range 1 .. 9 and compute πx.
 		
 		int x=RandomNumbers.getRandomInt(1,9);
-		System.out.println("Randomlly generated value of x is : " + x);
-		System.out.println("π the power of x is: " + Math.pow(3.14, x));
+		System.out.println("Randomly generated value of x is : " + x);
+		System.out.println("π the power of x is: " + Math.pow(Math.PI, x));
 		
 		//2. get a random number y in the range 3 .. 14 and compute yπ
 		
 		int y=RandomNumbers.getRandomInt(3,14);
-		System.out.println("Randomlly generated value of y is : " + y);
-		System.out.println("y the power of π is: "+ Math.pow(y, 3.14));		
+		System.out.println("Randomly generated value of y is : " + y);
+		System.out.println("y the power of π is: "+ Math.pow(y, Math.PI));		
 		
 	}
 
